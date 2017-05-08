6d5135aca5d9fadc5f3d0449f3b8217e19146e45
Matthew Lam <matthewy28@gmail.com>
2017-05-09 02:39:05 +0800

    simplify send and receive SQS messages

---
+++ aws-lambda/functions/receiveSQSMessage/index.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk')
+const config = require('./config/config')
+
+AWS.config.loadFromPath('./config/aws.json')
+const SQS = new AWS.SQS({ apiVersion: '2012-11-05' })
+const Lambda = new AWS.Lambda({ apiVersion: '2015-03-31' })
+
+// Your queue URL stored in the queueUrl environment variable
+const QUEUE_URL = config.queueUrl
+
+exports.handle = (event, context, callback) => {
+  var params = {
+     QueueUrl: QUEUE_URL,
+     VisibilityTimeout: 600
+ };
+
+ SQS.receiveMessage(params, (err, data) => {
+     if (err) {
+       callback(err)
+     }
+
+     callback(null, data)
+   })
+ }
